4403714fa83ce7e78ee6b179bc91fbc435e6288c
yellbuy <19892257@qq.com>
2019-11-26 14:50:16 +0800

    增加node解析

--- httpes.go
+++ httpes.go
@@ -1,10 +1,12 @@
 package httpes
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
-	"io/ioutil"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Jar struct {
@@ -30,7 +32,7 @@ type ResponseBody struct {
 var client = &http.Client{nil, nil, new(Jar), 99999999999992}
 
 func (http *Http) Post(url string, body string) (*ResponseBody, error) {
-	rep, err := client.Post(http.Host+url, "application/x-www-form-urlencoded", strings.NewReader(body));
+	rep, err := client.Post(http.Host+url, "application/x-www-form-urlencoded", strings.NewReader(body))
 	if err == nil {
 		body, _ := ioutil.ReadAll(rep.Body)
 		resp := &ResponseBody{Body: body, StatusCode: rep.StatusCode}
@@ -41,8 +43,44 @@ func (http *Http) Post(url string, body string) (*ResponseBody, error) {
 	}
 }
 
+func (http *Http) GetForDocument(url string) (int, *goquery.Document, error) {
+	rep, err := client.Get(http.Host + url)
+	if err == nil {
+		doc, err := goquery.NewDocumentFromReader(rep.Body)
+
+		defer rep.Body.Close()
+		return rep.StatusCode, doc, err
+	} else {
+		return 0, nil, err
+	}
+}
+
+func (http *Http) PostFormForDocument(url string, values url.Values) (int, *goquery.Document, error) {
+	rep, err := client.PostForm(http.Host+url, values)
+	if err == nil {
+		doc, err := goquery.NewDocumentFromReader(rep.Body)
+
+		defer rep.Body.Close()
+		return rep.StatusCode, doc, err
+	} else {
+		return 0, nil, err
+	}
+}
+
+func (http *Http) PostForDocument(url string, body string) (int, *goquery.Document, error) {
+	rep, err := client.Post(http.Host+url, "application/x-www-form-urlencoded", strings.NewReader(body))
+	if err == nil {
+		doc, err := goquery.NewDocumentFromReader(rep.Body)
+
+		defer rep.Body.Close()
+		return rep.StatusCode, doc, err
+	} else {
+		return 0, nil, err
+	}
+}
+
 func (http *Http) Get(url string) (*ResponseBody, error) {
-	rep, err := client.Get(http.Host + url);
+	rep, err := client.Get(http.Host + url)
 	if err == nil {
 		body, _ := ioutil.ReadAll(rep.Body)
 		resp := &ResponseBody{Body: body, StatusCode: rep.StatusCode}
